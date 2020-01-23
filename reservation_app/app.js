fcf18258963536b7a1cf81628e456a1802285e6b
Tim Scheepens <timscheepens@gmail.com>
2020-01-23 16:40:31 +0100

    Small changes in reservation app, mainly style

--- reservation_app/app.js
+++ reservation_app/app.js
@@ -1,19 +1,36 @@
 $(document).ready(function(){
+
     // app html
     var app_html=`
         <div class="container">
+
                 <div class="page-steps">
                     <div class="circle"><p>1</p></div>
                     <h1 class="page_steps">Choose dates</h1>
                 </div>
 
-                <!-- this is where the contents will be shown. -->
-                <div id="page-content"></div>
+                
+                <div id="page-content">
+                    <!-- this is where the contents will be shown. -->
+                </div>
+
         </div>`;
+
     // inject to 'app' in index.html
     $("#reservation").html(app_html);
+
+    var chooseDates_html=`
+                                   
+                        `;
+
+
+
+    $("#page-content").html(chooseDates_html);
+
 });
  
+
+
 // change page title
 function changePageTitle(page_title){
     // change page title
@@ -22,19 +39,21 @@ function changePageTitle(page_title){
     document.title=page_title;
 }
  
-// function to make form values to json format
-$.fn.serializeObject = function(){
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
+
+
+// // function to make form values to json format
+// $.fn.serializeObject = function(){
+//     var o = {};
+//     var a = this.serializeArray();
+//     $.each(a, function() {
+//         if (o[this.name] !== undefined) {
+//             if (!o[this.name].push) {
+//                 o[this.name] = [o[this.name]];
+//             }
+//             o[this.name].push(this.value || '');
+//         } else {
+//             o[this.name] = this.value || '';
+//         }
+//     });
+//     return o;
+// };
--- reservation_app/formsteps/form_step1.js
+++ reservation_app/formsteps/form_step1.js
@@ -1,21 +1,13 @@
 // product list html
-function readProductsTemplate(data, keywords){
+function setStepOneContent(data, keywords){
     var read_products_html=`
-        <!-- start table -->
-        <table class='table table-bordered table-hover'>`;
- 
-            // loop through returned list of data
-            $.each(data.records, function(key, val) {
-                // creating new table row per record
-                read_products_html+=`<tr>
-                    <td>testing</td>
-                </tr>`;
-            });
-        // end table
-        read_products_html+=`</table>`;
+                            
+                            `;
+
+
 
     // inject to 'page-content' of our app
     $("#page-content").html(read_products_html);
 
-    changePageTitle("testing");
+    changePageTitle("Choose dates");
 }
