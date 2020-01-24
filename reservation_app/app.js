42be96254d4a7efbe7869918af4953b05ac83e27
Tim Scheepens <timscheepens@gmail.com>
2020-01-24 11:42:06 +0100

    updated step1 client-side page

--- reservation_app/app.js
+++ reservation_app/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 
     // app html
@@ -11,21 +12,44 @@ $(document).ready(function(){
 
                 
                 <div id="page-content">
-                    <!-- this is where the contents will be shown. -->
+                    <div class="content_box">
+                        <form action="form_step2.js" method="POST">
+                            <div class="input_group">
+                                <div class="form-group">
+                                    <div class="">
+                                        <label for="startDate"><span class="calendar">Start date:</span></label>
+                                        <input class="date_input" type="date" id="startDate" name="startDate" value="" />
+            
+                                        <label for="numberNights"><span class="calendar">Number of nights:</span></label>
+                                        <input type="number" name="numberNights" min="1" max="14" value="0">
+            
+                                        <label for="endDate"><span class="calendar">End date:</span></label>
+                                        <input class="date_input" type="date" id="endDate" name="endDate" />
+                                    </div>
+                                </div>
+                            </div>
+            
+                            <div class="input_group">
+                                <div class="form-group">
+                                    <div class="">
+                                        <label for="numberNights"><span class="form_label_span">Number of guests:</span></label>
+                                        <input type="number" name="numberGuests" min="1" max="2" value="0">
+            
+                                        <label for="numberNights"><span class="form_label_span">Number of rooms:</span></label>
+                                        <input type="number" name="numberRooms" min="1" max="1" value="0">
+                                    </div>
+                                </div>
+                            </div>
+            
+                            <button type="submit" class="btn btn-primary">Next step</button>
+                        </form>
+                    </div>
                 </div>
-
-        </div>`;
+        </div>
+        `;
 
     // inject to 'app' in index.html
     $("#reservation").html(app_html);
-
-    var chooseDates_html=`
-                                   
-                        `;
-
-
-
-    $("#page-content").html(chooseDates_html);
 
 });
  
