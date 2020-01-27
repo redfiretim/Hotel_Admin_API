804b54282db2199c68258ae1cd1a2a098f8e5d63
pjiepsma <pieter.iepsma@gmail.com>
2020-01-27 11:10:32 +0100

    Add class this to variables

--- api/config/config.class.php
+++ api/config/config.class.php
@@ -1,16 +1,16 @@
 <?php
-
 /* IMPORTANT
 As of PHP 5.4 you can also use the short array syntax, which replaces array() with [].
 So DONT use the short hand syntax [] for arrays.
 */
-
-class Config {
+class Config
+{
     // Specifies database properties.
     private $host = 'localhost';
     private $db_name = 'reservation_system';
     private $username = 'root';
     private $password = '';
+
     // Specifies table properties.
     private $a = 'accommodations';
     private $at = 'accommodation_types';
@@ -25,80 +25,82 @@ class Config {
 
     // Defines methods to get query content.
     // Tables needed:
-    getTables($action) {
+    public function getTables($action)
+    {
         switch ($action) {
             case 'read_reservations':
-                return strval($r.', '.$c.', '.$a.', '.$e);
+                return strval($this->r.', '.$this->c.', '.$this->a.', '.$this->e);
                 break;
             case 'read_one_reservation':
-                return strval($a.', '.$at.', '.$am.', '.$apa.', '.$ci.', '.$co.', '.$c.', '.$e.', '.$et.', '.$r);
+                return strval($this->a.', '.$this->at.', '.$this->am.', '.$this->apa.', '.$this->ci.', '.$this->co.', '.$this->c.', '.$this->e.', '.$this->et.', '.$this->r);
                 break;
-        } 
+        }
     }
+
     // Columns needed:
-    getColumns($action) {
+    public function getColumns($action)
+    {
         switch ($action) {
             case 'read_reservations':
                 return array(
-                    $r.'.id',
-                    $c.'.first_name',
-                    $c.'.last_name',
-                    $a.'.room_num',
-                    $r.'.total_price',
-                    $r.'.check_in_date',
-                    $r.'.check_out_date',
+                    $this->r.'.id',
+                    $this->c.'.first_name',
+                    $this->c.'.last_name',
+                    $this->a.'.room_num',
+                    $this->r.'.total_price',
+                    $this->r.'.check_in_date',
+                    $this->r.'.check_out_date',
                 );
                 break;
             case 'read_one_reservation':
-                return array (
-                    $r.'.id',
-                    $r.'.booking_date',
-                    $r.'.num_of_pers',
-                    $r.'.check_in_date',
-                    $r.'.check_out_date',
-                    $r.'.num_of_nights',
-                    $r.'.total_price',
-                    $c.'.first_name',
-                    $c.'.last_name',
-                    $c.'.email',
-                    $c.'.phone_num',
-                    $e.'.name',
-                    $e.'.zipcode',
-                    $e.'.street_name',
-                    $e.'.street_num',
-                    $co.'.name',
-                    $ci.'.name',
-                    $at.'.name',
-                    $a.'.price_per_night',
-                    $a.'.room_num',
+                return array(
+                    $this->r.'.id',
+                    $this->r.'.booking_date',
+                    $this->r.'.num_of_pers',
+                    $this->r.'.check_in_date',
+                    $this->r.'.check_out_date',
+                    $this->r.'.num_of_nights',
+                    $this->r.'.total_price',
+                    $this->c.'.first_name',
+                    $this->c.'.last_name',
+                    $this->c.'.email',
+                    $this->c.'.phone_num',
+                    $this->e.'.name',
+                    $this->e.'.zipcode',
+                    $this->e.'.street_name',
+                    $this->e.'.street_num',
+                    $this->co.'.name',
+                    $this->ci.'.name',
+                    $this->at.'.name',
+                    $this->a.'.price_per_night',
+                    $this->a.'.room_num',
                 );
                 break;
-        } 
+        }
     }
 
     // Constant conditions (table joiners) needed:
-    getConstConditions($action) {
+    public function getConstConditions($action)
+    {
         switch ($action) {
             case 'read_reservations':
                 return array(
-                    $r.'.customer_id = '.$c.'.id',
-                    $r.'.accommodation_id = '.$a.'.id',
-                    $a.'.establishment_id = '.$e.'.id',
+                    $this->r.'.customer_id = '.$this->c.'.id',
+                    $this->r.'.accommodation_id = '.$this->a.'.id',
+                    $this->a.'.establishment_id = '.$this->e.'.id',
                 );
                 break;
             case 'read_one_reservation':
                 return array(
-                    $r.'.customer_id = '.$c.'.id',
-                    $r.'.accommodation_id = '.$a.'.id',
-                    $a.'.establishment_id = '.$e.'.id',
-                    $a.'.accommodation_type_id = '.$at.'.id',
-                    $at.'.establishment_type_id = '.$et.'.id',
-                    $e.'.country_id = '.$co.'.id',
-                    $e.'.cities_id = '.$ci.'.id',
+                    $this->r.'.customer_id = '.$this->c.'.id',
+                    $this->r.'.accommodation_id = '.$this->a.'.id',
+                    $this->a.'.establishment_id = '.$this->e.'.id',
+                    $this->a.'.accommodation_type_id = '.$this->at.'.id',
+                    $this->at.'.establishment_type_id = '.$this->et.'.id',
+                    $this->e.'.country_id = '.$this->co.'.id',
+                    $this->e.'.city_id = '.$this->ci.'.id',
                 );
                 break;
-        } 
+        }
     }
 }
-
-
