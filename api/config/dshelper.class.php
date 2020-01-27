88eac76cfdc06052a837543cc63fdec6f2a4909a
pjiepsma <pieter.iepsma@gmail.com>
2020-01-27 13:43:07 +0100

    Fix missing )

--- api/config/dshelper.class.php
+++ api/config/dshelper.class.php
@@ -1,23 +1,26 @@
 <?php
 
-class DSHelper {
+class DSHelper
+{
     // Datasource handle property.
     private $dsh;
 
     // Catch handle via constructor.
-    public function __construct($dsh) {
+    public function __construct($dsh)
+    {
         $this->dsh = $dsh;
     }
 
     // CREATE METHOD.
-    public function create($table, $data) {
+    public function create($table, $data)
+    {
         // Defines a column and prepared value array.
         $cols = array();
         $preps = array();
         // Splits the data parameter into columns and prepared values and pushes them into arrays.
         foreach ($data as $column => $value) {
-            array_push($cols, $column;
-            array_push($preps, ':'.$column;
+            array_push($cols, $column);
+            array_push($preps, ':'.$column);
         }
         // Defines sql statement: columns and values are extracted from cols and preps arrays.
         $sql = 'INSERT INTO '.$table.' ('.implode(', ', $cols).') VALUES ('.implode(', ', $preps).')';
@@ -33,20 +36,21 @@ class DSHelper {
     }
 
     // READ METHOD.
-    public function read($tables, $columns, $var_conditions, $const_conditions = '',) {
+    public function read($tables, $columns, $var_conditions, $const_conditions = '')
+    {
         // Defines a conditions variable.
         $conditions = '';
         // Changes var_conditions into prepared conditions.
         foreach ($var_conditions as $column => $value) {
-                $conditions .= $column.'=:'.$column;
-                if (next($var_conditions)) {
-                    $conditions .= ' AND ';
-                }
+            $conditions .= $column.'=:'.$column;
+            if (next($var_conditions)) {
+                $conditions .= ' AND ';
             }
         }
+
         // Checks for const_conditions (fixed conditions that are needed to join tables).
         if (isset($const_conditions) && $const_conditions != '') {
-            $conditions .= ' AND '
+            $conditions .= ' AND ';
             foreach ($const_conditions as $const_condition) {
                 $conditions .= $const_condition;
                 if (next($const_conditions)) {
@@ -68,13 +72,14 @@ class DSHelper {
     }
 
     // UPDATE METHOD.
-    public function update($table, $data, $conditions) {
+    public function update($table, $data, $conditions)
+    {
         // Defines a settings and where_clause variable.
         $settings = array();
         $where_clause = '';
         // Redefines key-value pairs into key-prepared value pairs and pushes them into the settings array.
         foreach ($data as $column => $value) {
-            array_push($settings, $column.'=:'.$column;
+            array_push($settings, $column.'=:'.$column);
         }
         // Changes conditions into prepared conditions.
         foreach ($conditions as $column => $value) {
@@ -95,11 +100,13 @@ class DSHelper {
         }
         // Executes statement.
         $stmt->execute();
+
         return;
     }
 
     // DELETE METHOD.
-    public function delete($table, $conditions) {
+    public function delete($table, $conditions)
+    {
         // Defines a where_clause variable.
         $where_clause = '';
         // Changes conditions into prepared conditions.
@@ -118,7 +125,7 @@ class DSHelper {
         }
         // Executes statement.
         $stmt->execute();
+
         return;
     }
 }
-?>
