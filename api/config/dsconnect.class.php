d378ebc116efaea813478abac7136fc2fb6b0ca0
Thomas Leunissen <thomas_leunissen@hotmail.com>
2020-01-27 11:28:53 +0100

    dbconnect rewritten because db credentials were moved to config.class.php

--- api/config/dsconnect.class.php
+++ api/config/dsconnect.class.php
@@ -1,18 +1,23 @@
 <?php
 
-class DSConnect
-{
+class DSConnect {
+    // Define datasource properties.
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
     public static $ds;
     private $dsc;
 
-    // specify your own database credentials
-    private $host = 'localhost';
-    private $db_name = 'reservation_system';
-    private $username = 'root';
-    private $password = '';
+    // Catch datasource credentials via constructor.
+    public function __construct($host, $db_name, $username, $password) {
+        $this->host = $host;
+        $this->db_name = $db_name;
+        $this->username = $username;
+        $this->password = $password;
+    }
 
-    private function DSConnect()
-    {
+    private function DSConnect() {
         try {
             $this->dsc = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, $this->password);
             $this->dsc->exec('set names utf8');
@@ -21,12 +26,10 @@ class DSConnect
         }
     }
 
-    public static function getInstance()
-    {
+    public static function getInstance($host, $db_name, $username, $password) {
         if (!isset(DSConnect::$ds)) {
-            DSConnect::$ds = new DSConnect();
+            DSConnect::$ds = new DSConnect($host, $db_name, $username, $password);
         }
-
         return DSConnect::$ds->dsc;
     }
 }
