451fca86f98ba8a45adacbce082c9eb6e41d6373
Thomas Leunissen <thomas_leunissen@hotmail.com>
2020-01-28 16:57:11 +0100

    ds credentials inserted by making use of scope resolution operator

--- api/config/dsconnect.class.php
+++ api/config/dsconnect.class.php
@@ -1,37 +1,43 @@
 <?php
-
 class DSConnect {
-    // Define datasource properties.
+    // PROPERTIES:
+    // DS object.
+    public static $ds;
+    // DS handle.
+    private $dsh;
+    // DS credentials.
     private $host;
     private $db_name;
     private $username;
-    private $password;
-    public static $ds;
-    private $dsc;
+    private $password; 
 
-    // Catch datasource credentials via constructor.
-    public function __construct($host, $db_name, $username, $password) {
-        $this->host = $host;
-        $this->db_name = $db_name;
-        $this->username = $username;
-        $this->password = $password;
+    // CONSTRUCTOR:
+    // Catches datasource credentials.
+    public function __construct() {
+        $this->host = Config::$host;
+        $this->db_name = Config::$db_name;
+        $this->username = Config::$username;
+        $this->password = Config::$password;
     }
 
+    // METHODS:
+    // Creates a data source handle.
     private function DSConnect() {
         try {
-            $this->dsc = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, $this->password);
-            $this->dsc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->dsc->exec('set names utf8');
-
+            $this->dsh = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, $this->password);
+            $this->dsh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->dsh->exec('set names utf8');
         } catch (PDOException $exception) {
             echo 'dscection error: '.$exception->getMessage();
         }
     }
 
-    public static function getInstance($host, $db_name, $username, $password) {
+    // Creates a DSConnect object inside the DSConnect class.
+    public static function getInstance() {
         if (!isset(DSConnect::$ds)) {
-            DSConnect::$ds = new DSConnect($host, $db_name, $username, $password);
+            DSConnect::$ds = new DSConnect();
         }
-        return DSConnect::$ds->dsc;
+        DSConnect::$ds->DSConnect();
+        return DSConnect::$ds->dsh; 
     }
 }
