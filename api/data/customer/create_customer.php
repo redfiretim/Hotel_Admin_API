53dd4d74445fbf1fcf64f67a2845cd77d83d0f8f
Thomas Leunissen <thomas_leunissen@hotmail.com>
2020-01-29 14:54:43 +0100

    regex added

--- api/data/customer/create_customer.php
+++ api/data/customer/create_customer.php
@@ -10,26 +10,36 @@ $last_name = $data->last_name;
 $email = $data->email;
 $phone_num = $data->phone_num;
 
-$customer_columns = $config->getColumns('read_customer');
-$customer_table = $config->getTables('create_customer');
-$var_conditions = array('email' => $email);
+// Validates data with regular expressions.
+if (preg_match('^([A-Za-z]{1,32}[ \-]?[A-Za-z]{1,32}){1,32}$',$first_name) && 
+    preg_match('^([A-Za-z]{1,32}[ \-]?[A-Za-z]{1,32}){1,32}$',$last_name) && 
+    preg_match('^([A-Za-z0-9]{1}[A-Za-z0-9\.\_\-]{0,63}@[A-Za-z0-9]{1,80}[\.]{1}[A-Za-z]{2,20}){0,150}$',$email) &&
+    preg_match('^[\+]{1}[1-9]{1}[0-9\-]{9,18}$|^[0-9\-]{10,20}$',$phone_num)) {
+        // Defines query content.
+        $customer_columns = $config->getColumns('read_customer');
+        $customer_table = $config->getTables('create_customer');
+        $var_conditions = array('email' => $email);
 
-//Check database if there is already a customer with that email
-$stmt = $dshelper->read($customer_table, $customer_columns, $var_conditions);
-$num = $stmt->rowCount();
-if ($num > 0) {
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $customer_id = $row['id'];
-    }
-    http_response_code(200);
+        //Check database if there is already a customer with that email
+        $stmt = $dshelper->read($customer_table, $customer_columns, $var_conditions);
+        $num = $stmt->rowCount();
+        if ($num > 0) {
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $customer_id = $row['id'];
+            }
+            http_response_code(200);
 
-    echo json_encode($customer_id);
-} else {  //Insert a new customer in the database
-    $customer_data = array('first_name' => "$first_name", 'last_name' => $last_name, 'email' => $email, 'phone_num' => $phone_num);
-    $customer_id = $dshelper->create($customer_table, $customer_data);
+            echo json_encode($customer_id);
+        } else {  //Insert a new customer in the database
+            $customer_data = array('first_name' => "$first_name", 'last_name' => $last_name, 'email' => $email, 'phone_num' => $phone_num);
+            $customer_id = $dshelper->create($customer_table, $customer_data);
 
-    // set response code - 201 CREATED
-    http_response_code(201);
+            // set response code - 201 CREATED
+            http_response_code(201);
 
-    echo json_encode($customer_id);
+            echo json_encode($customer_id);
+        }
+} else {
+    http_response_code(404);
+    echo json_encode(array('message' => 'Looks like something went wrong'));
 }
