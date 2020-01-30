136083aea3af1dc98c31f202d40100a58d902480
Thomas Leunissen <thomas_leunissen@hotmail.com>
2020-01-30 12:22:18 +0100

    regex added

--- api/data/reservation/update_reservation.php
+++ api/data/reservation/update_reservation.php
@@ -1,8 +1,6 @@
 <?php 
-	
-include_once '../index.php';
 
-	//Reservation ID: 
+//Reservation ID: 
 if (isset($_GET['id'])) {
     $reservation_id = $_GET['id'];	
 	
@@ -13,36 +11,51 @@ if (isset($_GET['id'])) {
 	$check_out_date = $data->check_out_date;
 	$room_num = $data->room_num; 
 	$total_price = $data->total_price; 
-	
-	//FIX: Get room to get accommodation_id room_num
-	
-	$update_reservation_array = array( 
-		'customers.first_name' => $first_name, 
-		'customers.last_name' => $last_name, 
-		'reservations.check_in_date' => $check_in_date, 
-		'reservations.check_out_date' => $check_out_date, 
-		'reservations.room_num' => $room_num,
-		'reservations.total_price' => $total_price 
-	); 
-	
-	$conditions_array = array(
-		'reservations.id' => $reservation_id, 
-		'reservations.customer_id' => 'customers.id' 	
-	); 
-	
-	 //update($table, $data, $conditions)
-	//$config = new Config(); 
-	//$tables = $config->getTables('update_reservation'); 
-	//$tables has already been defined in the index.php
-	
-	$stmt = $dshelper->update($tables, $update_reservation_array, $conditions_array); 
-	//Return a value to the index.php script to set the right response code.
-	echo $stmt; 
-	
-	 // repsonse received, updated
-    http_response_code(200);
 
-    echo json_encode(array('message' => 'Reservation updated.'));
+	if (
+	preg_match('/^([A-Za-z]{1,32}[ \-]?[A-Za-z]{1,32}){1,32}$/',$first_name) &&
+	preg_match('/^([A-Za-z]{1,32}[ \-]?[A-Za-z]{1,32}){1,32}$/',$last_name) &&
+	preg_match('/^[\d]{4}[\-](0[1-9]|1[012])[\-](0[1-9]|[12][0-9]|3[01])$/',$check_in_date) && 
+	preg_match('/^[\d]{4}[\-](0[1-9]|1[012])[\-](0[1-9]|[12][0-9]|3[01])$/',$check_out_date) &&
+	preg_match('/^[0-9]{1,4}$/',$room_num) &&
+	preg_match('/^[1-9]{1}[0-9]{0,3}$/',$total_price) &&
+	$check_in_date < $check_out_date) {
+	
+		//FIX: Get room to get accommodation_id room_num
+		
+		$update_reservation_array = array( 
+			'customers.first_name' => $first_name, 
+			'customers.last_name' => $last_name, 
+			'reservations.check_in_date' => $check_in_date, 
+			'reservations.check_out_date' => $check_out_date, 
+			'reservations.room_num' => $room_num,
+			'reservations.total_price' => $total_price 
+		); 
+		
+		$conditions_array = array(
+			'reservations.id' => $reservation_id, 
+			'reservations.customer_id' => 'customers.id' 	
+		); 
+		
+		//update($table, $data, $conditions)
+		//$config = new Config(); 
+		//$tables = $config->getTables('update_reservation'); 
+		//$tables has already been defined in the index.php
+		
+		$stmt = $dshelper->update($tables, $update_reservation_array, $conditions_array); 
+		//Return a value to the index.php script to set the right response code.
+		echo $stmt; 
+		
+		// repsonse received, updated
+		http_response_code(200);
+
+		echo json_encode(array('message' => 'Reservation updated.'));
+	} else {
+		// bad request
+		http_response_code(400);
+		// tell the user that something went wrong
+		echo json_encode(array('message' => 'Looks like something went wrong'));
+	}
 } else {
     // repsonse received, no data found
     http_response_code(200);
