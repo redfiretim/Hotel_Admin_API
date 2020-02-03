3876c086e1a4be145d65fd7c8c80c553070e2f96
Tim Scheepens <timscheepens@gmail.com>
2020-02-03 14:09:57 +0100

    made cope of step2 and step3 for testing purposes

---
+++ reservation_app/formsteps/form_step2 copy.js
@@ -0,0 +1,95 @@
+
+// function form_step2(){
+function showAvailableRoom(result){
+    var json_data = result["records"];
+    console.log(json_data);
+    readRoomsTemplate(json_data, "");
+    console.log("in show available room functie")
+}
+
+// function to show list of products
+function showRooms(json_data){
+    // get list of products from the API
+    $.getJSON(json_data, function(data){
+        //html for listing products
+        readRoomsTemplate(data, "");
+    });
+}
+
+function readRoomsTemplate(data){
+    // start of html object
+    var step_content = `<div class="room-container"><form name="room_type_form" id="room_type_form">`;
+        // loop through data to generate content radio blocks
+        $.each(data, function(index, data) {
+            console.log(data);
+
+            var bedType;
+        
+
+            if((data.name) == "Standard triple room")
+            {
+                bedType = "Separate beds";
+            }
+            if((data.name) == "Standard double room")
+            {
+                bedType = "Queen size bed";
+            }
+            if((data.name) == "Deluxe double room")
+            {
+                bedType = "King size bed";
+            }
+            // else{
+            //     bedType = "Unknown";
+            // }
+
+            console.log(bedType);
+
+
+            step_content += `
+                <label class="room_block" onclick="">
+                    <input type="radio" name="radioname" value="` + data.name + `" />
+                    
+                    <div class="row">
+                        <div class="col-xs-6 col-md-4">
+                            <img id="room_tumbnail" src="../reservation_app/assets/images/`+ data.image_one +`" alt="Room"/>
+                        </div>
+                        <div class="col-xs-6 col-md-4 room_properties">                        
+                            <h4>` + data.name + ` <span class="glyphicon glyphicon-ok"></span></h4>
+                            <p><span class="glyphicon glyphicon-user properties"></span> ` + data.max_pers + `</p>
+                            <p><span class="glyphicon glyphicon-bed properties"></span> ` + bedType + `</p>
+                            <p><span class="glyphicon glyphicon-plus"></span> ` + data.amenities + `</p>
+                        </div>
+                        <div class="col-xs-6 col-md-4">
+                            <p>`+ data.description +`</p>
+                        </div>
+                    </div>
+                </label>`;  
+        });
+        // submit button + back button
+        step_content += ` <button type='submit' class='btn btn-primary page-button2'>
+                                <span class='glyphicon glyphicon-chevron-right pull-right'></span>Choose room
+                        </button>
+                        <a type="button" class="btn btn-link" href="../reservation_app/index.html">< One step back</a>`;
+        step_content += `</form></div>`;
+
+
+    // inject html to 'page-content' of our app
+    $("#page-content").html(step_content);
+
+    // change content titelbar step number and titel
+    changePageCircle("2");
+    changePageTitle("Choose room");
+};
+
+
+
+$(document).on('submit', '#room_type_form', function(){
+    
+    // Put form user input into Local Storage
+    if(saveForm() == true){
+        showUserDetailsForm();
+    }
+
+
+});
+
---
+++ reservation_app/formsteps/form_step3 copy.js
@@ -0,0 +1,46 @@
+function showUserDetailsForm(){
+  
+    console.log("hoi nogmaals");
+    // CALLBACK NEEDED
+    var step_content=`   
+        <form name='validate_Userform' id='create-reservation-form' action='#' method='post' border='0'>
+            <div class="form-group">
+                <label for="firstname">First name:*</label>
+                <input type="text" name='first_name' class="form-control"  pattern="[A-Za-z]{1,32}" required placeholder="Enter first name">
+            </div>
+            <div class="form-group">
+                <label for="lastname">Last name:*</label>
+                <input type="text" name='last_name' class="form-control" pattern="[A-Za-z]{1,32}" required placeholder="Enter last name">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address:*</label>
+                <input type="email" name='email' class="form-control" pattern='(.+)@(.+)\.(.+){2,}' required placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="phone">Phone number:*</label>
+                <input type="number" name='phonenumber' class="form-control" pattern='[\+]{1}[1-9]{1}[0-9\-]{9,18}$|^[0-9]{1}[0-9\-]{9,20}' required placeholder="Enter phonenumber">
+                <small id="Help" class="form-text text-muted">* are required</small>
+            </div>
+        </form>
+        
+        <button type='submit' class='btn btn-primary page-button3'>
+            <span class='glyphicon glyphicon-chevron-right pull-right'></span> To summary
+        </button>
+        <button type="button" class="btn btn-link page-button">< One step back</button>`;
+
+        // inject html to 'page-content' of our app
+    $("#page-content").html(step_content);
+    
+    $(document).on('click', '.page-button3' , function(){
+        return validateFormStep3();
+        //return saveForm();
+    });
+
+    changePageCircle("3");
+    changePageTitle("Fill in contact details");
+
+
+// $(document).on('submit', '#create-product-form', function(){
+
+// });
+};
